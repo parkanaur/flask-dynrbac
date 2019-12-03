3f8bcaf3d8078393880412be19eef044147c3c37
Daniil Kraynov <dykrajno@mts.ru>
2019-12-03 12:27:52 +0300

    [test] Added a dummy test, updated setup.py

---
+++ flask_dynrbac/tests/extension_init_test.py
@@ -0,0 +1,16 @@
+import unittest
+from flask import Flask
+import flask_dynrbac
+
+
+class ExtensionInitTestCase(unittest.TestCase):
+    """Unit tests for extension initialization"""
+    def test_simple_initialization(self):
+        app = Flask(__name__)
+        rbac = flask_dynrbac.DynRBAC(app)
+
+        self.assertIsNotNone(rbac)
+
+
+if __name__ == '__main__':
+    unittest.main()
