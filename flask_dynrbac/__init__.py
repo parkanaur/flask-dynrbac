937d9cadc8bd9d52bedf392990fb098a5233744c
Daniil Kraynov <dykrajno@mts.ru>
2020-01-27 18:23:07 +0300

    [feat] register app extension

--- flask_dynrbac/__init__.py
+++ flask_dynrbac/__init__.py
@@ -8,12 +8,22 @@ class DynRBAC(object):
     role handling."""
 
     def __init__(self, app=None):
+        """Initializes, configures and binds the extension instance to an app"""
         self.app = app
 
         if app is not None:
             self.init_app(app)
 
     def init_app(self, app):
+        """Configures the extension to be used later"""
+        self._validate_requirements(app)
+
+        app.extensions['dynrbac'] = self
+
+    def _validate_requirements(self, app):
+        """ Checks whether the supplied app fulfills the extension/domain model requirements.
+            Throws a warning if some requirement is not met."""
+
         if app.extensions.get('sqlalchemy') is None:
             raise util.SQLAlchemyNotSuppliedWarning('Flask-SQLAlchemy is not initialized before DynRBAC. '
                                                     'DynRBAC requires SQLAlchemy for role and permission data '
